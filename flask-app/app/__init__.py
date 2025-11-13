a8986212b5308748a29a1b3ff4594e6178d3d8e5
hisao5232 <hisao52321983@gmail.com>
2025-11-13 20:53:25 +0900

    fix https-access

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -8,6 +8,9 @@ def create_app():
     app = Flask(__name__)
     app.secret_key = os.getenv("SECRET_KEY", "supersecret")
 
+    # --- Traefik 経由のHTTPSを正しく認識させる ---
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
     # DB初期化
     init_db()
 
